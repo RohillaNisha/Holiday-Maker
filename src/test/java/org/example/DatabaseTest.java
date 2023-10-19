e09a7fe96cca36fe8eb1d209ae2563bd4adc1978
Nisha <nisha.rohilla@gmail.com>
2023-10-19 11:30:59 +0200

    removed junit dependency from databaseTest classs

--- src/test/java/org/example/DatabaseTest.java
+++ src/test/java/org/example/DatabaseTest.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.junit.Before;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,9 +7,6 @@ class DatabaseTest {
 
     private Database db;
 
-    @Before
-    public void setUp(){
 
-    }
 
 }
